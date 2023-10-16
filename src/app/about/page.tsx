163ab5293554026bece336d22063c1432ca9ceb1
JamesFlosse <james.flosse@gmail.com>
2023-10-16 10:07:26 +0200

    Install de packages supplémentaires pour eslint et modif du code pour coller aux régles d'airbnb

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <div>
+        <p>
+          A propos de soso
+        </p>
+      </div>
+    </main>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <p>
+          je suis la page
+        </p>
+      </div>
+    </main>
+  );
+}
